ace74dc672421fbe9a33137005b4238533616c97
vitor-bertuzzi <vitorbertuzzi18@gmail.com>
2026-05-25 10:32:19 -0300

    Alcool ou gasolina

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const[precoEtanol, setPrecoEtanol] = useState("");
+  const[precoGasolina, setPrecoGasolina] = useState("");
+  const[resultado, setResultado] = useState("");
+  function calcular(){
+    const calculo = precoEtanol/precoGasolina;
+    if(calculo<0.7){
+      setResultado("Etanol é a melhor opcao");
+    }else{
+      setResultado("Gasolina é a melhor opcao")
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}>ALCOOL OU GASOLINA</Text>
+      <TextInput style={styles.input}
+      value={precoEtanol} 
+      onChangeText = {setPrecoEtanol}
+      placeholder = "Digite o preco do etanol"/>
+      <TextInput style={styles.input}
+      value={precoGasolina} 
+      onChangeText = {setPrecoGasolina}
+      placeholder = "Digite o preco da Gasolina"/>
+      <TouchableOpacity onPress={calcular} style={styles.botao}>
+      <Text style={styles.botaoTexto}>VERIFICAR VANTAGEM</Text>
+      </TouchableOpacity>
+      <Text>{resultado}</Text>
     </View>
   );
 }
@@ -16,5 +38,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  titulo: {
+    fontSize: 20,
+    alignItems: 'center'
+  },
+
+  input: {
+    height: 20
+  },
+
+  botao: {
+   
+  },
+
+  botaoTexto: {
+    
   },
 });
