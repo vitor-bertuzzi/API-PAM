569b1a74eeede84812b469810904177499b8abb4
Vítor Bertuzzi <vitorbertuzzi18@gmail.com>
2026-05-25 12:02:49 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import PostoImage from './src/components/posto.jpg';
 import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
   const[precoEtanol, setPrecoEtanol] = useState("");
@@ -8,22 +10,25 @@ export default function App() {
   function calcular(){
     const calculo = precoEtanol/precoGasolina;
     if(calculo<0.7){
-      setResultado("Etanol é a melhor opcao");
+      setResultado("Abasteça com: ETANOL\n O etanol está custando "+ calculo+"% da gasolina");
     }else{
-      setResultado("Gasolina é a melhor opcao")
+      setResultado("Abasteça com: GASOLINA\n O etanol está custando "+ calculo+"% da gasolina")
     }
   }
   return (
+    
     <View style={styles.container}>
+     
       <Text style={styles.titulo}>ALCOOL OU GASOLINA</Text>
+      <Image source={PostoImage} style={styles.Image} />
       <TextInput style={styles.input}
       value={precoEtanol} 
       onChangeText = {setPrecoEtanol}
-      placeholder = "Digite o preco do etanol"/>
+      placeholder = "Preço do Etanol:"/>
       <TextInput style={styles.input}
       value={precoGasolina} 
       onChangeText = {setPrecoGasolina}
-      placeholder = "Digite o preco da Gasolina"/>
+      placeholder = "Preco da Gasolina:"/>
       <TouchableOpacity onPress={calcular} style={styles.botao}>
       <Text style={styles.botaoTexto}>VERIFICAR VANTAGEM</Text>
       </TouchableOpacity>
@@ -55,5 +60,10 @@ const styles = StyleSheet.create({
 
   botaoTexto: {
     
+  },
+
+  Image: {
+    width: 150,
+    height: 150,
   },
 });
